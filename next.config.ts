4645c6b94a9def732b1a5f24923bf1ea6711e4aa
Henrique Borges <henriqueborgeshbr@proton.me>
2025-06-06 13:09:55 -0300

    Remove configurações relacionadas ao GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: process.env.NODE_ENV === "development" ? undefined : "/website"
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
